aff0b42332b1d3dc0f555ed16341a8da0bb233d2
Hussam Alafandi <hosam.alafandi@gmail.com>
2024-05-24 17:15:38 +0200

    Add ep length to BPO

--- fancy_gym/envs/mujoco/box_pushing/box_pushing_obstacle_env.py
+++ fancy_gym/envs/mujoco/box_pushing/box_pushing_obstacle_env.py
@@ -412,6 +412,10 @@ class BoxPushingObstacleEnvBase(MujocoEnv, utils.EzPickle):
     def _get_rotation_dist(self, box_quat, target_quat, target_quat2, target_quat3, target_quat4):
         raise NotImplementedError
 
+    @property
+    def episode_length(self):
+        return MAX_EPISODE_STEPS_BOX_PUSHING
+
 
 class BoxPushingObstacleDense(BoxPushingObstacleEnvBase):
     def __init__(self, frame_skip: int = 10, **kwargs):
